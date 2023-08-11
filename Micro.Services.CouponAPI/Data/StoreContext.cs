90f94c457946875130307b93a01dc24764a475a0
Manish Kumar Pandey <896849@cognizant.com>
2023-08-11 16:09:20 +0530

    worked on AppDbCOntext, created database

---
+++ Micro.Services.CouponAPI/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Micro.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micro.Services.CouponAPI.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+    }
+}
