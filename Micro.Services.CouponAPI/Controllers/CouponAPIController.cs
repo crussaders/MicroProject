5ff3b2e36cc813abf92ebe55da2b80ef89c09d82
Manish Kumar Pandey <896849@cognizant.com>
2023-08-11 16:52:37 +0530

    Get all and get by id fucntion in couponapicontroller

---
+++ Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,51 @@
+﻿using Micro.Services.CouponAPI.Data;
+using Micro.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly StoreContext _db;
+        public CouponAPIController(StoreContext db)
+        {   
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object GetById(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=>u.CouponId==id);
+                return objList;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+    }
+}
