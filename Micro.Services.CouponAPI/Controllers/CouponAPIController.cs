db86fc119006eb3d0c8d3e1c9ce2bca73e06ade0
Manish Kumar Pandey <896849@cognizant.com>
2023-08-13 11:21:31 +0530

    mapper config

--- Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -55,5 +55,47 @@ namespace Micro.Services.CouponAPI.Controllers
             }
             return _response;
         }
+
+        [HttpGet]
+        [Route("GetByCode{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.FirstOrDefault(u => u.CouponCode.ToLower() == code.ToLower());
+                if(obj == null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
