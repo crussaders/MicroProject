9c1df3922ef8a0672ec37fc3d2632403c91351a9
Manish Kumar Pandey <896849@cognizant.com>
2023-08-15 12:56:40 +0530

    Base service implementation

--- Micro.Web/Service/BaseService.cs
+++ Micro.Web/Service/BaseService.cs
@@ -1,13 +1,80 @@
 ﻿using Micro.Web.Models;
 using Micro.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static Micro.Web.Utility.SD;
 
 namespace Micro.Web.Service
 {
     public class BaseService : IBaseService
     {
-        public Task<ResponseDto> SendAsync(RequestDto requestDto)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
         {
-            throw new NotImplementedException();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MicroAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+
+                //token
+
+                message.RequestUri = new Uri(requestDto.Url);
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+            } catch (Exception ex)
+            {
+                var dto = new ResponseDto { 
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+                };
+
+                return dto;
+            }
         }
     }
 }
