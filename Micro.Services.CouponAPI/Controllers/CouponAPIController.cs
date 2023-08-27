6768ba565d3511071c7902aebc2f4e3721ab9eb9
Manish Kumar Pandey <896849@cognizant.com>
2023-08-27 12:53:17 +0530

    Coupon Index, Coupon View

--- Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Micro.Services.CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
---
+++ Micro.Web/Controllers/CouponController.cs
@@ -0,0 +1,34 @@
+﻿using Micro.Web.Models;
+using Micro.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Micro.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+
+            if(response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+    }
+}
